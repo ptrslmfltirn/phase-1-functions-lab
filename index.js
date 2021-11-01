6d0bb2aeb11b8512397a9dfe0f22435e29212a08
Peter Lamson <ptrlmsn@gmail.com>
2021-11-01 13:59:31 -0400

    commit tree a95120f5cd52ab84a8a53d1be26080a6f6320adb

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks (pickup) {
+   let distance = 42-pickup;
+   let abdistance = Math.abs (distance);
+   return abdistance;
+}
+function distanceFromHqInFeet (pickup) {
+    let distance = 42-pickup;
+    let abdistance = Math.abs (distance);
+    let feetdistance = abdistance * 264;
+    return feetdistance;
+}
+function distanceTravelledInFeet (pickup, dropoff) {
+    let distance = Math.abs (dropoff - pickup);
+    let distanceinfeet = distance * 264;
+    return distanceinfeet;
+}
+function calculatesFarePrice (start, destination) {
+   let totaldistance = distanceTravelledInFeet (start, destination)
+   if (totaldistance < 400) {
+       return 0
+   }
+   else if (totaldistance > 2500) {
+       return `cannot travel that far`
+   }
+   else if (totaldistance > 2000) {
+       return 25
+   }
+   else {
+       return ((totaldistance-400) * .02)
+   }
+}
